e9883a11672bc323ef814faedffc6972ee564d0a
Jonas Umland <umland.jonas@gmail.com>
2021-01-21 20:34:49 +0100

    parse args, convert to networkx graph

--- src/graph/graph.py
+++ src/graph/graph.py
@@ -1,6 +1,7 @@
 from typing import List
 
 import pandas as pd
+import networkx as nx
 
 from src.variables.variable import Variable
 
@@ -23,3 +24,11 @@ class Graph:
         for variable in self.variables:
             df[variable.idx] = variable.sample(df=df, num_observations=num_observations)
         return df
+
+    def to_networkx_graph(self):
+        nx_graph = nx.DiGraph()
+        for var in self.__get_top_sort_variables():
+            nx_graph.add_node(var.idx)
+            for parent in var.parents:
+                nx_graph.add_edge(parent.idx, var.idx)
+        return nx_graph
---
+++ test/graph/test_graph.py
@@ -0,0 +1,37 @@
+from src.graph.graph import Graph
+from src.noise import DiscreteNoise
+from src.prob_distributions import BinomialDistribution, CustomDiscreteDistribution, UniformDiscreteDistribution
+from src.variables.discrete_variable import DiscreteVariable
+import networkx as nx
+
+
+def create_simple_discrete_model2():
+    A = DiscreteVariable(idx=0, num_values=2, noise=DiscreteNoise(prob_distribution=BinomialDistribution(probability=.25)))
+    B = DiscreteVariable(idx=1, num_values=3, noise=DiscreteNoise(UniformDiscreteDistribution(num_values=3)))
+    C = DiscreteVariable(idx=2, num_values=3, parents=[A, B],
+                         mapping={
+                             (0, 0): 1,
+                             (0, 1): 2,
+                             (0, 2): 1,
+                             (1, 0): 2,
+                             (1, 1): 2,
+                             (1, 2): 2
+                         },
+                         noise=DiscreteNoise(
+                             prob_distribution=CustomDiscreteDistribution(probs=[.5, .2, .3])))
+    variables = [A, B, C]
+    return Graph(variables=variables)
+
+
+def test_to_networkx_graph():
+    expected = nx.DiGraph()
+    expected.add_node(0)
+    expected.add_node(1)
+    expected.add_node(2)
+    expected.add_edge(0, 2)
+    expected.add_edge(1, 2)
+
+    graph = create_simple_discrete_model2()
+    actual = graph.to_networkx_graph()
+
+    assert nx.is_isomorphic(expected, actual)
