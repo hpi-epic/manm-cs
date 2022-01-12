dfeabe3c0e2c8d9e782c45a8c91ed63bb8837a44
Ben Hurdelhey <Ben.Hurdelhey@vm-midea03.eaalab.hpi.uni-potsdam.de>
2022-01-12 23:23:28 +0000

    improve test script

--- test/test_manm_cs_pypi.py
+++ test/test_manm_cs_pypi.py
@@ -13,9 +13,10 @@ def test_manm_cs_pypi():
 
     os.system(
         "python3 -m pip install . &&" + # install dependencies from actual pip because they are not on testpypi
+        "mkdir -p test_pypi && cd test_pypi && " +
         "python3 -m pip uninstall manm-cs &&" + # uninstall the package itself so we can install it from testpypi
         "python3 -m pip install -i https://test.pypi.org/simple/ manm-cs &&" + # install latest manm-cs from testpypi
-        f"mkdir -p test_pypi && cd test_pypi && python3 -m manm_cs --num_nodes 10 --edge_density 0.5 --num_samples 10000 --discrete_node_ratio 0.5 --output_ground_truth_file \"{ground_truth}\" --output_samples_file \"{samples}\"")
+        f"python3 -m manm_cs --num_nodes 10 --edge_density 0.5 --num_samples 10000 --discrete_node_ratio 0.5 --output_ground_truth_file \"{ground_truth}\" --output_samples_file \"{samples}\"")
 
     assert os.path.isfile(ground_truth)
     assert os.path.isfile(samples)
